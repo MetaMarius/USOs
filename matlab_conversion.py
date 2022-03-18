7142da28608aed6878483badd360a49b52aee86d
Jakab Pilaszanovich <jakab.pilaszanovich@gmail.com>
2022-03-18 12:14:53 +0100

    Load all sounds from directory

--- matlab_conversion.py
+++ matlab_conversion.py
@@ -1,3 +1,21 @@
+import slab
+import os
+from os import listdir
+import pathlib
+from os.path import isfile, join
+from random import randrange
 
+slab.set_default_samplerate(48000)
 
+DIR = pathlib.Path(os.getcwd())
 
+file_path = DIR / 'mitsubishi_wavs'
+
+file_names = [file_path / f for f in listdir(file_path)
+                  if isfile(join(file_path, f))
+                  and not f.startswith('.')
+                  and f.endswith('.wav')]
+
+file_names.sort()
+
+sound_files = [slab.Sound(file_names[randrange(0, len(file_names))]) for i in range(0, 6)]
