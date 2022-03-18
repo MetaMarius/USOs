799771d57c9e2637d42d2bb2e5321934e5b47b5e
Jakab Pilaszanovich <jakab.pilaszanovich@gmail.com>
2022-03-18 15:50:35 +0100

    Plays back a concatenated series of random length sound snippets

--- matlab_conversion.py
+++ matlab_conversion.py
@@ -1,9 +1,11 @@
+import random
 import slab
 import os
 from os import listdir
 import pathlib
 from os.path import isfile, join
 from random import randrange
+import copy
 
 slab.set_default_samplerate(48000)
 
@@ -18,4 +20,20 @@ file_names = [file_path / f for f in listdir(file_path)
 
 file_names.sort()
 
-sound_files = [slab.Sound(file_names[randrange(0, len(file_names))]) for i in range(0, 6)]
+sound_files = [slab.Binaural(file_names[randrange(0, len(file_names))]) for i in range(0, 6)]
+
+output = slab.Binaural.silence(duration=1)
+
+for sound_file in sound_files:
+    snippet = copy.deepcopy(sound_file)
+    sound_length = len(sound_file)
+    min = int(0.02 * 48000)
+    max = int(0.2 * 48000)
+    snippet_length = random.randrange(min, max)
+    offset = random.randrange(0, sound_length - snippet_length)
+    snippet_length = slab.Signal.in_samples(snippet_length, sound_file.samplerate)
+    offset = slab.Signal.in_samples(offset, sound_file.samplerate)
+    snippet.data = sound_file.data[offset:offset + snippet_length]
+    output = slab.Binaural.sequence(output, snippet)
+
+output.play()
